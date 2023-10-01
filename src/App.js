06158c7814438795cfe359f06dd7a59c5500c4d0
lebang11 <lebangnong@gmail.com>
2023-10-01 21:34:59 +0200

    firebase practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import logo from './logo.svg';
+import { upload } from '@testing-library/user-event/dist/upload';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { storage } from './firebase';
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+import {v4} from 'uuid';
 
 function App() {
-  return <div>Firebase</div>;
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+
+  const imageListRef = ref(storage, 'images/')
+  
+  const uploadImage = () => {
+    if (imageUpload === null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert('Image Uploaded')
+    })
+  }
+
+  useEffect(() => {
+      listAll(imageListRef)
+      .then((res) => {
+        res.items.forEach((item) => {
+          getDownloadURL(item)
+          .then((url) => {
+            setImageList((prev) => [...prev, url])
+          })
+        })
+      })
+  }, []);
+
+
+  return (
+    <div>
+      <input type='file' onChange={(event) => {
+        setImageUpload(event.target.files[0]);
+        }}/>
+      <button onClick={uploadImage}>Upload Image</button>
+      {imageList.map((url) => {
+        return (
+          <div>
+            <a href={url}>Download</a>
+          </div>
+      )})}
+    </div>
+  )
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from 'firebase/storage';
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAjLzlQ75TvWKUcc2TG37muEg9SZJVZV8",
+  authDomain: "team-hub-18735.firebaseapp.com",
+  projectId: "team-hub-18735",
+  storageBucket: "team-hub-18735.appspot.com",
+  messagingSenderId: "795963822854",
+  appId: "1:795963822854:web:99c767d5fd05dc3d024487",
+  measurementId: "G-6GMR573WMH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const storage = getStorage(app)
